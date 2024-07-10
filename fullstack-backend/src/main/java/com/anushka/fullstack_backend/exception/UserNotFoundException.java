59e7ce395fe77bcddb6d4cdbc0feafe300072b39
Anushka Mulik <80084874+AnushkaM02@users.noreply.github.com>
2024-07-10 23:44:36 +0530

    full stack application

---
+++ fullstack-backend/src/main/java/com/anushka/fullstack_backend/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.anushka.fullstack_backend.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(Long id){
+        super("Could not find user with id " + id);
+    }
+}
---
+++ fullstack-frontend/src/layout/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav
+        className="navbar navbar-expand-lg bg-body-tertiary bg-primary"
+        data-bs-theme="dark"
+      >
+        <div className="container-fluid">
+          <Link className="navbar-brand" href="#" to="/">
+            Full Stack Application
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <Link className="btn btn-outline-light" to="/adduser">
+            Add User
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+}
